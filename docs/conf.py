c0cb1e4545075d22fd299b8d36a3b528a7620f11
Gregor Lichtner <gregor.lichtner@charite.de>
2018-07-10 15:16:09 +0200

    updated sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,15 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+from unittest.mock import MagicMock
 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['numpy', 'turboreg']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 exec(open('../pystackreg/version.py').read())
 
