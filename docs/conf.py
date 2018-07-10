f27ba62a9afbb5d517ec94bb6a68cec5b9968f04
Gregor Lichtner <gregor.lichtner@charite.de>
2018-07-10 15:27:52 +0200

    updated sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'turboreg']
+MOCK_MODULES = ['tqdm', 'tqdm.tqdm', 'pystackreg', 'pystackreg.turboreg']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 exec(open('../pystackreg/version.py').read())
